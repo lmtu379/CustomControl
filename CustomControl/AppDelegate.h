ab52acc4ca8b1e59d54c9c4548b3f0167dab3173
OOKBEE10 <OOKBEE10@minhtu-pc.local>
2016-06-20 17:10:13 +0700

    Initial Commit

---
+++ CustomControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomControl
+//
+//  Created by Thỏ on 6/20/16.
+//  Copyright © 2016 Thỏ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
